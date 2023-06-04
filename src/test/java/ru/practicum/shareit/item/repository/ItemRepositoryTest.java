18e643ca27fae5c7dd2adcd2d0ddb711bdddcbd2
Pavel Kabanov <pkabanov.d@yandex.ru>
2023-06-04 19:27:19 +0300

    fix: codestyle

--- src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.PageRequest;
 import ru.practicum.shareit.booking.BookingStatus;
 import ru.practicum.shareit.booking.model.Booking;
